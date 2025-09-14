6a543cea97e664493ded2a41691efca73f06ede8
Pavan <reddypavan2k@gmail.com>
2025-09-14 17:02:10 -0500

    seccond commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { serves } from "inngest/next";
+import { inngest } from "../../../inngest/client";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [
+        syncUserCreation,
+        syncUserUpdation,
+        syncUserDeletion,
+    ],
+});
