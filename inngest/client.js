67e64982bdb719f97813393cc6d1e36c4eeb8e5e
Pavan <reddypavan2k@gmail.com>
2025-09-15 16:03:13 -0500

    client

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
 
-
-export const inngest = new Inngest({ id: "gocart-ecommerce" });
+export const inngest = new Inngest({
+  id: "gocart-ecommerce",          // unique identifier for your app
+  name: "GoCart E-Commerce App",   // human-readable name (shows in Inngest UI)
+});
